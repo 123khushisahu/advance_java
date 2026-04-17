b98e8587daa49881753c564897e28cc09432d89f
123khushisahu <khushisahu9580@gmail.com>
2026-04-17 16:27:56 +0530

    done

---
+++ App7/src/main/java/com/dao/AddEmpDAO.java
@@ -0,0 +1,28 @@
+package com.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import com.bean.EmployeeBean;
+
+public class AddEmpDAO {
+	public int insertEmpData(EmployeeBean eb)
+	{
+		int rowCount=0;
+		try {
+			Connection con=DBConnect.getCon();
+			PreparedStatement pstmt=con.prepareStatement("insert into employee values(?,?,?,?,?)");
+			
+			pstmt.setString(1, eb.getEmp_id());
+			pstmt.setString(2, eb.getEmp_fname());
+			pstmt.setString(3, eb.getEmp_lname());
+			pstmt.setInt(4, eb.getEmp_sal());		
+			pstmt.setString(5, eb.getEmp_addr());
+			rowCount=pstmt.executeUpdate();
+			}catch (Exception e) {
+				e.printStackTrace();		
+			}
+			return rowCount;
+		}
+
+}
---
+++ App7/src/main/java/com/servlet/AddEmpServlet.java
@@ -0,0 +1,35 @@
+package com.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bean.EmployeeBean;
+import com.dao.AddEmpDAO;
+
+@WebServlet("/acs")
+public class AddEmpServlet extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+	{
+		EmployeeBean ebean=new EmployeeBean();
+		
+		ebean.setEmp_id(req.getParameter("eid"));
+		ebean.setEmp_fname(req.getParameter("efname"));
+		ebean.setEmp_lname(req.getParameter("elname"));
+		ebean.setEmp_sal(Integer.parseInt(req.getParameter("esal")));
+		ebean.setEmp_addr(req.getParameter("addr"));
+		AddEmpDAO dao_Obj=new AddEmpDAO();
+		int rowCount=dao_Obj.insertEmpData(ebean);
+		if(rowCount==0)
+			System.out.println("Employee data is not inserted!!!!!");
+		else
+			System.out.println("Employee data inserted successfully!!!!");
+		
+	}
+
+}
